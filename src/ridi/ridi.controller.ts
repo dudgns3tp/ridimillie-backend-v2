3d37140ef2120212f0f524e8f3b31154609080b4
최영훈 <c_yh0327@naver.com>
2021-12-17 22:35:43 +0900

    [chore] add serverless package

--- src/ridi/ridi.controller.ts
+++ src/ridi/ridi.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { RidiService } from './ridi.service';
 import { CreateRidiDto } from './dto/create-ridi.dto';
 import { UpdateRidiDto } from './dto/update-ridi.dto';
