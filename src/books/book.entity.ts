02c476955cece05b83e97d08f78bdb7112388af6
최영훈 <c_yh0327@naver.com>
2021-12-18 19:40:05 +0900

    [feat] bookEntity

--- src/books/book.entity.ts
+++ src/books/book.entity.ts
@@ -1,1 +1,12 @@
-export class Book {}
+enum bookType {
+  subscribe,
+  purchase,
+}
+export class Book {
+  platform: string; // hashKey
+  id: number; // rangeKey globalIndex
+  title: string; // index
+  viewCount: number;
+  type: bookType;
+  url: string;
+}
