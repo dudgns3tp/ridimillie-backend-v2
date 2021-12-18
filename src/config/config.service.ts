37254254794c51d7c184e368d29884bffa660409
최영훈 <c_yh0327@naver.com>
2021-12-18 19:44:35 +0900

    [chore] add stage env

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -6,10 +6,12 @@ export class EnvironmentService {
   naverClientId: string;
   naverClientSecret: string;
   tableName: string;
+  stage: string;
   constructor(private configService: ConfigService) {
     this.naverClientId = this.configService.get<string>('naverClientId');
     this.naverClientSecret =
       this.configService.get<string>('naverClientSecret');
     this.tableName = this.configService.get<string>('tableName');
+    this.stage = this.configService.get<string>('stage');
   }
 }
